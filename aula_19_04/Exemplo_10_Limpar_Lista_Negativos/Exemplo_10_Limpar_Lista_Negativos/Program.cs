4bc1fcc9326ded30aa8bb0e42e273b5811bbbbc0
Alexandre Vaz Roriz <201354@iesb.br>
2022-04-19 10:50:52 -0300

    atualização

---
+++ aula_19_04/Exemplo_10_Limpar_Lista_Negativos/Exemplo_10_Limpar_Lista_Negativos/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_10_Limpar_Lista_Negativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Gerar lista de 50 números inteiros aleatórios entre -100 e 100
+            Random rnd = new Random();
+            List<int> numerosAleatorios = new List<int>();
+            List<int> numerosPositivos = new List<int>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                numerosAleatorios.Add(rnd.Next(-100, 100));
+            }
+
+            // Imprimindo a lista criada
+            foreach (var item in numerosAleatorios)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+            // Limpar a lista dos números negativos
+            for (int i = 0; i < numerosAleatorios.Count; i++)
+            {
+                if (numerosAleatorios[i] >= 0)
+                {
+                    numerosPositivos.Add(numerosAleatorios[i]);
+                }
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Lista de números positivos");
+            foreach (var item in numerosPositivos)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ aula_19_04/Exemplo_9_Metodos_Collections/Exemplo_9_Metodos_Collections/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exemplo_9_Metodos_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> herois = new List<string> { "Homem-Aranha", "Batman", "Homem de Ferro",
+                "Thor", "Hulk", "Capitão América" };
+
+            foreach (var heroi in herois)
+            {
+                Console.WriteLine(heroi);
+            }
+            Console.Write("\n");
+
+
+            Console.WriteLine("Método Count (Quantidade de elementos na lista):");
+            Console.WriteLine(herois.Count() + "\n");
+
+            Console.WriteLine("Método Add (Doutor Estranho):");
+            herois.Add("Doutor Estranho");
+
+            foreach (var heroi in herois)
+            {
+                Console.WriteLine(heroi);
+            }
+            Console.Write("\n");
+
+
+            Console.WriteLine("Método Insert(posição 0: 'Thanos'):");
+            herois.Insert(0, "Thanos");
+            foreach (var heroi in herois)
+            {
+                Console.WriteLine(heroi);
+            }
+            Console.Write("\n");
+
+            Console.WriteLine("Método Remove('Hulk'):");
+            herois.Remove("Hulk");
+            foreach (var heroi in herois)
+            {
+                Console.WriteLine(heroi);
+            }
+            Console.Write("\n");
+
+            Console.WriteLine("Método RemoveAt(posição 0: 'Thanos'):");
+            herois.RemoveAt(0);
+            foreach (var heroi in herois)
+            {
+                Console.WriteLine(heroi);
+            }
+            Console.Write("\n");
+
+            //    //Console.WriteLine("Método Clear(remove todos os elementos da lista):");
+            //    //herois.Clear();
+            //    //imprimirLista(herois);
+
+            Console.WriteLine("Método IndexOF 'Thanos':");
+            Console.WriteLine(herois.IndexOf("Thanos"));
+
+            Console.WriteLine("\nMétodo IndexOF 'Homem de Ferro':");
+            Console.WriteLine(herois.IndexOf("Homem de Ferro"));
+
+            Console.WriteLine("Método Reverse:");
+            herois.Reverse();
+            foreach (var heroi in herois)
+            {
+                Console.WriteLine(heroi);
+            }
+            Console.Write("\n");
+
+            Console.WriteLine("\nMétodo Sort:");
+            herois.Sort();
+            foreach (var heroi in herois)
+            {
+                Console.WriteLine(heroi);
+            }
+            Console.Write("\n");
+
+
+            Console.WriteLine("Método Clear(remove todos os elementos da lista):");
+            herois.Clear();
+            foreach (var heroi in herois)
+            {
+                Console.WriteLine(heroi);
+            }
+            Console.Write("\n");
+
+            Console.ReadLine();
+     
+        }
+    }
+}
