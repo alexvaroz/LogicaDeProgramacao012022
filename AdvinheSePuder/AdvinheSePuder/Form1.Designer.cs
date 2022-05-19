01e3c5399a8805b3aadea92ef70eee0f4d77b0b2
Alexvaroz <alexandre.vaz@gmail.com>
2022-05-19 00:55:58 -0300

    Concluido

---
+++ AdvinheSePuder/AdvinheSePuder/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace AdvinheSePuder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Pacifico", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(-34, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 119);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Advinhe se puder!!!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Pacifico", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(116, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Jogar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Pacifico", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Location = new System.Drawing.Point(116, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Pacifico", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label2.Location = new System.Drawing.Point(39, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 67);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Você tem 5 tentativas, sendo que o número secreto está entre 0 e 50...";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(99, 204);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 52);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown_1);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.Location = new System.Drawing.Point(42, 358);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Tentativas já efetuadas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(42, 391);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(356, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Advinhe se puder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AdvinheSePuder/AdvinheSePuder/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvinheSePuder
+{
+    public partial class Form1 : Form
+    {
+        int numeroSecreto;
+        int tentativa;
+        int numeroTentativa;
+        int numeroMaximoTentativas = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CarregarConfiguraçãoInicial();
+
+        }
+
+        private void CarregarConfiguraçãoInicial()
+        {
+            textBox1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CarregarJogo();
+
+        }
+
+        private void CarregarJogo()
+        {
+            button1.Visible = false;
+            button2.Visible = false;
+            textBox1.Visible = true;
+            textBox1.Focus();
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            label4.Text = "";
+            tentativa = 1;
+            DefinirNumeroSecreto();
+        }
+
+        private void DefinirNumeroSecreto()
+        {
+            Random rnd = new Random();
+            numeroSecreto = rnd.Next(0, 51);
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            MessageBox.Show("Saiu");
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+        }
+
+        private void AvaliarTentativa(int numeroTentativa)
+        {
+            if (tentativa <= numeroMaximoTentativas)
+            {
+                if (numeroSecreto > numeroTentativa)
+                {
+
+                    MessageBox.Show("O número secreto é mais alto...");
+                }
+                else if (numeroSecreto < numeroTentativa)
+                {
+                    MessageBox.Show("O número secreto é menor...");
+                }
+                else
+                {
+                    MessageBox.Show($"Você acertou!!!\n O número secreto é {numeroSecreto}");
+                    FinalizarJogo();
+                }
+            } else
+            {
+                MessageBox.Show("Tentativas esgotadas... Game Over... \n" +
+                    $"O número secreto era {numeroSecreto}");
+                FinalizarJogo();
+            }
+        }
+
+        private void FinalizarJogo()
+        {
+            button1.Visible = true;
+            button2.Visible = true;
+            textBox1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+        }
+
+        private void textBox1_Validating(object sender, CancelEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+           
+        }
+
+        private void textBox1_KeyDown_1(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                tentativa++;
+                label4.Text = label4.Text + textBox1.Text + ", ";
+                numeroTentativa = int.Parse(textBox1.Text);
+                AvaliarTentativa(numeroTentativa);
+                e.Handled = true;
+                textBox1.Clear();
+            }
+        }
+
+
+    }
+}
