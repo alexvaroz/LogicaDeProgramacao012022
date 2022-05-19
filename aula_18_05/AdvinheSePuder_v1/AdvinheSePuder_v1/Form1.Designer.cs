173de977b4adb5cb3a3da92fb344b722662e5c5d
Alexandre Vaz Roriz <201354@iesb.br>
2022-05-19 10:46:19 -0300

    atualizacao

---
+++ aula_18_05/AdvinheSePuder_v1/AdvinheSePuder_v1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace AdvinheSePuder_v1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnJogar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.txbTentativa = new System.Windows.Forms.TextBox();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.lblTentativas = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(38, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adivinhe se puder";
+            // 
+            // btnJogar
+            // 
+            this.btnJogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogar.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnJogar.Location = new System.Drawing.Point(93, 147);
+            this.btnJogar.Name = "btnJogar";
+            this.btnJogar.Size = new System.Drawing.Size(83, 56);
+            this.btnJogar.TabIndex = 1;
+            this.btnJogar.Text = "Jogar";
+            this.btnJogar.UseVisualStyleBackColor = true;
+            this.btnJogar.Click += new System.EventHandler(this.btnJogar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnSair.Location = new System.Drawing.Point(93, 210);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(83, 56);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblInfo.Location = new System.Drawing.Point(-23, 76);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(330, 76);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "Foi sorteado um número \r\nentre 0 e 50... \r\nVocê tem 5 tentativas \r\npara adivinhar" +
+    "...";
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblInfo.Visible = false;
+            // 
+            // txbTentativa
+            // 
+            this.txbTentativa.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txbTentativa.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbTentativa.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.txbTentativa.Location = new System.Drawing.Point(93, 160);
+            this.txbTentativa.Multiline = true;
+            this.txbTentativa.Name = "txbTentativa";
+            this.txbTentativa.Size = new System.Drawing.Size(83, 33);
+            this.txbTentativa.TabIndex = 4;
+            this.txbTentativa.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txbTentativa.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txbTentativa_KeyDown);
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.Location = new System.Drawing.Point(23, 76);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(249, 68);
+            this.lblGameOver.TabIndex = 5;
+            this.lblGameOver.Text = "Obrigado por jogar o \"Adivinhe se puder\" ...\r\nDeseja jogar novamente? ";
+            this.lblGameOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblGameOver.Visible = false;
+            // 
+            // lblTentativas
+            // 
+            this.lblTentativas.AutoSize = true;
+            this.lblTentativas.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTentativas.Location = new System.Drawing.Point(23, 295);
+            this.lblTentativas.Name = "lblTentativas";
+            this.lblTentativas.Size = new System.Drawing.Size(89, 16);
+            this.lblTentativas.TabIndex = 6;
+            this.lblTentativas.Text = "Tentativas: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(284, 349);
+            this.Controls.Add(this.lblTentativas);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.txbTentativa);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnJogar);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Adivinhe se puder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnJogar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.TextBox txbTentativa;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.Label lblTentativas;
+    }
+}
+
---
+++ aula_18_05/AdvinheSePuder_v1/AdvinheSePuder_v1/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdvinheSePuder_v1
+{
+    public partial class Form1 : Form
+    {
+        int numeroSecreto;
+        int chute;
+        int chances;
+        public Form1()
+        {
+            InitializeComponent();
+            lblInfo.Visible = false;
+            txbTentativa.Visible = false;
+            lblTentativas.Visible = false;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnJogar_Click(object sender, EventArgs e)
+        {
+            IncializarJogo();
+        }
+
+        private void IncializarJogo()
+        {
+            btnSair.Visible = false;
+            btnJogar.Visible = false;
+            lblInfo.Visible = true;
+            txbTentativa.Visible = true;
+            txbTentativa.Focus();
+            Random rnd = new Random();
+            numeroSecreto = rnd.Next(0, 51);
+            chances = 5;
+            lblGameOver.Visible = false;
+            lblTentativas.Visible = true;
+        }
+
+        private void txbTentativa_KeyDown(object sender, KeyEventArgs e)
+        {
+           if (e.KeyCode == Keys.Enter)
+            {
+                chances--;
+                chute = int.Parse(txbTentativa.Text);
+                lblTentativas.Text = lblTentativas.Text + 
+                    txbTentativa.Text + ", ";
+                txbTentativa.Clear();
+                AvaliarChute(chute);
+                VerificarSeGameOver();
+            }
+        }
+
+        private void VerificarSeGameOver()
+        {
+            if (chances <= 0)
+            {
+                MessageBox.Show("Você não conseguiu adivinhar o número \n" +
+                    $"nas suas 5 chances... \n Era {numeroSecreto} \n Que pena... \n GAME OVER!!!");
+                lblGameOver.Visible = true;
+                btnSair.Visible = true;
+                btnJogar.Visible = true;
+                lblInfo.Visible = false;
+                txbTentativa.Visible = false;
+                lblTentativas.Text = "Tentativas: ";
+                lblTentativas.Visible = false;
+            } 
+        }
+
+        private void AvaliarChute(int chute)
+        {
+            if (chute > numeroSecreto)
+            {
+                MessageBox.Show($"O número secreto é menor que {chute} ");
+            }
+            else if (chute < numeroSecreto)
+            {
+                MessageBox.Show($"O número secreto é maior que {chute} ");
+            }
+            else
+            {
+                MessageBox.Show($"O número secreto é {chute} \n" +
+                   "Parabéns, você adivinhou!!");
+            }
+        }
+    }
+}
